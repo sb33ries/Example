562992f5685fae2ecd7a7a927353af05589ce9cf
sb33ries <124833574+sb33ries@users.noreply.github.com>
2024-04-17 13:16:03 -0400

    done with example1

--- Example_01/basic.py
+++ Example_01/basic.py
@@ -7,11 +7,31 @@ from datetime import datetime as dt
 import numpy as np
 
 TITLE = "TITLE"
+TAU = 2*np.pi
+FIG_ID = "figure-one"
+
+def run_app() -> None:
+    my_app = Dash(__name__)
+    print(f"Started running at {str(dt.now())}")
+    
+    my_app.title = TITLE
+    apply_main_layout(my_app)
+    my_app.run(debug = True)
+
+def make_figure() -> go.Figure:
+    x = np.linspace(0, 10, 500)
+    y = np.sin(TAU*x)
+
+    fig = px.scatter(None, x, y)
+
+    return fig
 
 def apply_main_layout(app: Dash) -> None:
     layout = html.Div(id = "main-div",
                       children = [html.H1("Welcome to my website"),
-                                  html.Hr()
+                                  html.Hr(),
+                                  html.H2("Here is a figure"),
+                                  dcc.Graph(id = FIG_ID, figure = make_figure())
                                   ]
                        
 
@@ -20,9 +40,4 @@ def apply_main_layout(app: Dash) -> None:
     return
 
 if __name__ == "__main__":
-    my_app = Dash(__name__)
-    print(f"Started running at {str(dt.now())}")
-    
-    my_app.title = TITLE
-    apply_main_layout(my_app)
-    my_app.run(debug = True)
+    run_app()
